b026c52c7e6b1eee482d2e09dc2be9c8065c3f9c
Emedigit <m.marcela.diaz.75@gmail.com>
2023-11-29 01:54:04 -0300

    First commmit

---
+++ main.py
@@ -0,0 +1,26 @@
+import pandas as pd
+from fastapi import FastAPI
+
+
+app = FastAPI()
+
+df_funcion1 = pd.read_parquet('data/df_funcion1.parquet')
+df_funcion2 = pd.read_parquet('data/df_funcion2.parquet')
+
+
+@app.get("/ PlayTimeGenre")
+def  PlayTimeGenre(genero: str):
+    '''
+      Debe devolver año con mas horas jugadas para dicho género.
+        
+    '''
+    # Filtra el dataframe por desarrollador de interés
+    data_filtrada = df_funcion1[df_funcion1['genero'] == genero]
+
+    resultado = data_filtrada['anio']
+    
+    result_dict = {
+        '{"Año de lanzamiento con más horas jugadas para Género {genero}" :}': resultado.to_dict()
+    }
+    
+    return result_dict
